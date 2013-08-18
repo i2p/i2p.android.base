ae2fa4dce7667e4eab09c17127dd02296a54d4d9
str4d <str4d@mail.i2p>
2013-08-18 12:46:13 +0000

    Fill I2PTunnel list with a Loader
    Until changes to the list of TunnelControllers can be monitored, the Loader
    refreshes the view every 10 seconds.

--- src/net/i2p/android/router/fragment/I2PTunnelFragment.java
+++ src/net/i2p/android/router/fragment/I2PTunnelFragment.java
@@ -1,23 +1,30 @@
 package net.i2p.android.router.fragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.i2p.android.router.R;
 import net.i2p.android.router.adapter.TunnelControllerAdapter;
+import net.i2p.android.router.loader.TunnelControllerLoader;
 import net.i2p.i2ptunnel.TunnelController;
 import net.i2p.i2ptunnel.TunnelControllerGroup;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class I2PTunnelFragment extends ListFragment {
+public class I2PTunnelFragment extends ListFragment
+        implements LoaderManager.LoaderCallbacks<List<TunnelController>> {
     public static final String SHOW_CLIENT_TUNNELS = "show_client_tunnels";
+
+    private static final int CLIENT_LOADER_ID = 1;
+    private static final int SERVER_LOADER_ID = 2;
 
     private TunnelControllerGroup mGroup;
     private TunnelControllerAdapter mAdapter;
+    private boolean mClientTunnels;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class I2PTunnelFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mAdapter = new TunnelControllerAdapter(getActivity());
+        mClientTunnels = getArguments().getBoolean(SHOW_CLIENT_TUNNELS);
 
         String error;
         try {
@@ -39,36 +47,45 @@ public class I2PTunnelFragment extends ListFragment {
             error = iae.toString();
         }
 
-        boolean clientTunnels = getArguments().getBoolean(SHOW_CLIENT_TUNNELS);
         if (mGroup == null) {
             setEmptyText(error);
         } else {
-            if (clientTunnels)
+            if (mClientTunnels)
                 setEmptyText("No configured client tunnels.");
             else
                 setEmptyText("No configured server tunnels.");
         }
-        mAdapter.setData(getControllers(clientTunnels));
+
         setListAdapter(mAdapter);
+        setListShown(false);
+
+        getLoaderManager().initLoader(mClientTunnels ? CLIENT_LOADER_ID
+                : SERVER_LOADER_ID, null, this);
     }
 
-    private List<TunnelController> getControllers(boolean clientTunnels) {
-        List<TunnelController> ret = new ArrayList<TunnelController>();
-        for (TunnelController controller : mGroup.getControllers())
-            if ( (clientTunnels && isClient(controller.getType())) ||
-                 (!clientTunnels && !isClient(controller.getType())) )
-                ret.add(controller);
-        return ret;
+    public Loader<List<TunnelController>> onCreateLoader(int id, Bundle args) {
+        return new TunnelControllerLoader(getActivity(), mGroup, mClientTunnels);
     }
 
-    private static boolean isClient(String type) {
-        return ( ("client".equals(type)) ||
-                 ("httpclient".equals(type)) ||
-                 ("sockstunnel".equals(type)) ||
-                 ("socksirctunnel".equals(type)) ||
-                 ("connectclient".equals(type)) ||
-                 ("streamrclient".equals(type)) ||
-                 ("ircclient".equals(type)));
+    public void onLoadFinished(Loader<List<TunnelController>> loader,
+            List<TunnelController> data) {
+        if (loader.getId() == (mClientTunnels ?
+                CLIENT_LOADER_ID : SERVER_LOADER_ID)) {
+            mAdapter.setData(data);
+
+            if (isResumed()) {
+                setListShown(true);
+            } else {
+                setListShownNoAnimation(true);
+            }
+        }
+    }
+
+    public void onLoaderReset(Loader<List<TunnelController>> loader) {
+        if (loader.getId() == (mClientTunnels ?
+                CLIENT_LOADER_ID : SERVER_LOADER_ID)) {
+            mAdapter.setData(null);
+        }
     }
 
     @Override
