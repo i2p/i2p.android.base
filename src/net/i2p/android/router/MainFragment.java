f207cf3116b933461ec36bda73c8710fdbc13970
str4d <str4d@mail.i2p>
2014-01-11 21:07:42 +0000

    Removed unneeded import

--- src/net/i2p/android/router/MainFragment.java
+++ src/net/i2p/android/router/MainFragment.java
@@ -1,7 +1,6 @@
 package net.i2p.android.router;
 
 import android.app.Activity;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
