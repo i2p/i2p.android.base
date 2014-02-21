e271dc90aed9fdc7a462caed20c12bd6ef12e96e
str4d <str4d@mail.i2p>
2014-02-21 00:13:03 +0000

    Removed License button from VersionDialog

--- src/net/i2p/android/router/dialog/VersionDialog.java
+++ src/net/i2p/android/router/dialog/VersionDialog.java
@@ -1,14 +1,12 @@
 package net.i2p.android.router.dialog;
 
 import net.i2p.android.router.I2PActivityBase;
-import net.i2p.android.router.LicenseActivity;
 import net.i2p.android.router.MainFragment;
 import net.i2p.android.router.R;
 import net.i2p.android.router.util.Util;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -27,14 +25,14 @@ public class VersionDialog extends DialogFragment {
             case DIALOG_NEW_INSTALL:
                 b.setMessage(R.string.welcome_new_install)
                  .setCancelable(false)
-                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
                         I2PActivityBase ab = (I2PActivityBase) getActivity();
                         ab.setPref(MainFragment.PREF_INSTALLED_VERSION, currentVersion);
                         dialog.dismiss();
                     }
-                }).setNeutralButton(R.string.label_release_notes, new DialogInterface.OnClickListener() {
+                }).setNegativeButton(R.string.label_release_notes, new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
                         I2PActivityBase ab = (I2PActivityBase) getActivity();
@@ -50,15 +48,6 @@ public class VersionDialog extends DialogFragment {
                                      .addToBackStack(null)
                                      .commit();
                     }
-                }).setNegativeButton(R.string.label_licenses, new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int id) {
-                        I2PActivityBase ab = (I2PActivityBase) getActivity();
-                        ab.setPref(MainFragment.PREF_INSTALLED_VERSION, currentVersion);
-                        dialog.dismiss();
-                        Intent intent = new Intent(getActivity(), LicenseActivity.class);
-                        startActivity(intent);
-                    }
                 });
                 rv = b.create();
                 break;
@@ -67,25 +56,19 @@ public class VersionDialog extends DialogFragment {
                 b.setMessage(getResources().getString(R.string.welcome_new_version) +
                              " " + currentVersion)
                  .setCancelable(true)
-                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
                         I2PActivityBase ab = (I2PActivityBase) getActivity();
                         ab.setPref(MainFragment.PREF_INSTALLED_VERSION, currentVersion);
-                        try {
-                            dialog.dismiss();
-                        } catch(Exception e) {
-                        }
+                        dialog.dismiss();
                     }
                 }).setNegativeButton(R.string.label_release_notes, new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
                         I2PActivityBase ab = (I2PActivityBase) getActivity();
                         ab.setPref(MainFragment.PREF_INSTALLED_VERSION, currentVersion);
-                        try {
-                            dialog.dismiss();
-                        } catch(Exception e) {
-                        }
+                        dialog.dismiss();
                         TextResourceDialog f = new TextResourceDialog();
                         Bundle args = new Bundle();
                         args.putInt(TextResourceDialog.TEXT_RESOURCE_ID, R.raw.releasenotes_txt);
