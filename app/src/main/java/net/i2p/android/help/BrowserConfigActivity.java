4d75ce7de139f9cefbb9b935e7a461479f1f6b61
str4d <str4d@mail.i2p>
2014-11-08 07:50:30 +0000

    Missing files

---
+++ app/src/main/java/net/i2p/android/help/BrowserConfigActivity.java
@@ -0,0 +1,105 @@
+package net.i2p.android.help;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import net.i2p.android.router.R;
+
+import java.lang.reflect.Field;
+
+public class BrowserConfigActivity extends ActionBarActivity implements
+        BrowserAdapter.OnBrowserSelectedListener {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    private boolean mDetailShowing;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        // Set the action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        if (findViewById(R.id.detail_fragment) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_fragment, new BrowserListFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDetailShowing) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            mDetailShowing = false;
+        }
+        super.onBackPressed();
+    }
+
+    // BrowserAdapter.OnBrowserSelected
+
+    @Override
+    public void onBrowserSelected(Browser browser) {
+        int file;
+        if (browser.isKnown) {
+            if (browser.isSupported) {
+                // Check for embedded browser
+                if (browser.packageName.startsWith("net.i2p.android"))
+                    file = R.raw.help_embedded_browser;
+                else {
+                    // Load the configuration guide for this browser
+                    try {
+                        String name = "help_" + browser.packageName.replace('.', '_');
+                        Class res = R.raw.class;
+                        Field field = res.getField(name);
+                        file = field.getInt(null);
+                    } catch (Exception e) {
+                        file = R.raw.help_unknown_browser;
+                    }
+                }
+            } else
+                file = R.raw.help_unsupported_browser;
+        } else
+            file = R.raw.help_unknown_browser;
+        HelpHtmlFragment configFrag = HelpHtmlFragment.newInstance(file);
+        if (mTwoPane) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment, configFrag)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_fragment, configFrag)
+                    .addToBackStack("config" + browser.packageName)
+                    .commit();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mDetailShowing = true;
+        }
+    }
+}
---
+++ app/src/main/java/net/i2p/android/help/HelpListFragment.java
@@ -0,0 +1,47 @@
+package net.i2p.android.help;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import net.i2p.android.router.R;
+
+public class HelpListFragment extends ListFragment {
+    OnEntrySelectedListener mEntrySelectedCallback;
+
+    // Container Activity must implement this interface
+    public interface OnEntrySelectedListener {
+        public void onEntrySelected(int entry);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mEntrySelectedCallback = (OnEntrySelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnEntrySelectedListener");
+        }
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setListAdapter(ArrayAdapter.createFromResource(getActivity(),
+                R.array.help_categories, android.R.layout.simple_list_item_1));
+    }
+
+    @Override
+    public void onListItemClick(ListView parent, View view, int pos, long id) {
+        super.onListItemClick(parent, view, pos, id);
+        mEntrySelectedCallback.onEntrySelected(pos);
+    }
+}
