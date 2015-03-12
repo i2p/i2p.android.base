6709bebc6f01a2b192b101e09b7b25b3e45ea8a7
str4d <str4d@mail.i2p>
2015-03-12 23:01:11 +0000

    EditTextPreference that updates its summary (from Bote)

---
+++ app/src/main/java/net/i2p/android/router/util/SummaryEditTextPreference.java
@@ -0,0 +1,28 @@
+package net.i2p.android.router.util;
+
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+public class SummaryEditTextPreference extends EditTextPreference {
+
+    public SummaryEditTextPreference(Context context) {
+        super(context);
+    }
+
+    public SummaryEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SummaryEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String summary = (String) super.getSummary();
+        if (summary == null)
+            summary = "%s";
+        return String.format(summary, getText());
+    }
+}
