f036544744b60771f3ff4c80ad505796ceadd350
str4d <str4d@mail.i2p>
2015-03-23 00:45:14 +0000

    Support numeric values for ListPreferences

---
+++ app/src/main/java/net/i2p/android/router/util/IntListPreference.java
@@ -0,0 +1,30 @@
+package net.i2p.android.router.util;
+
+import android.content.Context;
+import android.preference.ListPreference;
+import android.util.AttributeSet;
+
+public class IntListPreference extends ListPreference {
+    public IntListPreference(Context context) {
+        super(context);
+    }
+
+    public IntListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected boolean persistString(String value) {
+        return value != null && persistInt(Integer.valueOf(value));
+    }
+
+    @Override
+    protected String getPersistedString(String defaultReturnValue) {
+        if(getSharedPreferences().contains(getKey())) {
+            int intValue = getPersistedInt(0);
+            return String.valueOf(intValue);
+        } else {
+            return defaultReturnValue;
+        }
+    }
+}
