0095777a63d34f532efd3bf25e14109547347231
str4d <str4d@mail.i2p>
2015-04-01 12:24:58 +0000

    Replaced missing menu items

--- app/src/main/java/net/i2p/android/router/ConsoleContainer.java
+++ app/src/main/java/net/i2p/android/router/ConsoleContainer.java
@@ -1,7 +1,10 @@
 package net.i2p.android.router;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,14 +14,20 @@ import android.view.ViewGroup;
 
 import net.i2p.android.router.dialog.AboutDialog;
 import net.i2p.android.router.dialog.TextResourceDialog;
+import net.i2p.android.router.log.LogActivity;
+import net.i2p.android.router.netdb.NetDbActivity;
 import net.i2p.android.router.service.State;
+import net.i2p.android.router.stats.PeersActivity;
+import net.i2p.android.router.stats.RateGraphActivity;
 
 public class ConsoleContainer extends Fragment {
     MainFragment mMainFragment = null;
+    Toolbar mSubToolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -31,6 +40,40 @@ public class ConsoleContainer extends Fragment {
             getChildFragmentManager().beginTransaction()
                     .add(R.id.main_fragment, mMainFragment).commit();
         }
+
+        mSubToolbar = (Toolbar) v.findViewById(R.id.sub_toolbar);
+        mSubToolbar.inflateMenu(R.menu.container_console_sub_actions);
+        setAdvancedVisibility();
+        mSubToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_news:
+                        Intent news = new Intent(getActivity(), NewsActivity.class);
+                        startActivity(news);
+                        return true;
+                    case R.id.action_logs:
+                        Intent log = new Intent(getActivity(), LogActivity.class);
+                        startActivity(log);
+                        return true;
+                    case R.id.action_graphs:
+                        Intent graphs = new Intent(getActivity(), RateGraphActivity.class);
+                        startActivity(graphs);
+                        return true;
+                    case R.id.action_peers:
+                        Intent peers = new Intent(getActivity(), PeersActivity.class);
+                        startActivity(peers);
+                        return true;
+                    case R.id.action_netdb:
+                        Intent netdb = new Intent(getActivity(), NetDbActivity.class);
+                        startActivity(netdb);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
         return v;
     }
 
@@ -40,25 +83,39 @@ public class ConsoleContainer extends Fragment {
     }
 
     @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        setAdvancedVisibility();
+    }
+
+    private void setAdvancedVisibility() {
+        boolean advanced = PreferenceManager.getDefaultSharedPreferences(getActivity()).getBoolean(
+                "i2pandroid.main.showStats", false);
+
+        mSubToolbar.getMenu().findItem(R.id.action_graphs).setVisible(advanced);
+        mSubToolbar.getMenu().findItem(R.id.action_peers).setVisible(advanced);
+        mSubToolbar.getMenu().findItem(R.id.action_netdb).setVisible(advanced);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.menu_about:
-            AboutDialog dialog = new AboutDialog();
-            dialog.show(getFragmentManager(), "about");
-            return true;
+            case R.id.menu_about:
+                AboutDialog dialog = new AboutDialog();
+                dialog.show(getFragmentManager(), "about");
+                return true;
 
-        case R.id.menu_help_release_notes:
-            TextResourceDialog rDdialog = new TextResourceDialog();
-            Bundle args = new Bundle();
-            args.putString(TextResourceDialog.TEXT_DIALOG_TITLE,
-                    getResources().getString(R.string.label_release_notes));
-            args.putInt(TextResourceDialog.TEXT_RESOURCE_ID, R.raw.releasenotes_txt);
-            rDdialog.setArguments(args);
-            rDdialog.show(getFragmentManager(), "release_notes");
-            return true;
+            case R.id.menu_help_release_notes:
+                TextResourceDialog rDdialog = new TextResourceDialog();
+                Bundle args = new Bundle();
+                args.putString(TextResourceDialog.TEXT_DIALOG_TITLE,
+                        getResources().getString(R.string.label_release_notes));
+                args.putInt(TextResourceDialog.TEXT_RESOURCE_ID, R.raw.releasenotes_txt);
+                rDdialog.setArguments(args);
+                rDdialog.show(getFragmentManager(), "release_notes");
+                return true;
 
-        default:
-            return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
