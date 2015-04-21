124692db8ef3bf75f3c6759e3995feb6b34d745a
str4d <str4d@mail.i2p>
2015-04-21 03:58:16 +0000

    Reverse the Lollipop transition when finishing TunnelDetailActivity

--- app/src/main/java/net/i2p/android/i2ptunnel/TunnelDetailActivity.java
+++ app/src/main/java/net/i2p/android/i2ptunnel/TunnelDetailActivity.java
@@ -2,21 +2,35 @@ package net.i2p.android.i2ptunnel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 import net.i2p.android.I2PActivityBase;
 import net.i2p.android.i2ptunnel.preferences.EditTunnelActivity;
 
 public class TunnelDetailActivity extends I2PActivityBase implements
         TunnelDetailFragment.TunnelDetailListener {
+    private boolean transitionReversed;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        transitionReversed = false;
 
         if (savedInstanceState == null) {
             int tunnelId = getIntent().getIntExtra(TunnelDetailFragment.TUNNEL_ID, 0);
             TunnelDetailFragment detailFrag = TunnelDetailFragment.newInstance(tunnelId);
             getSupportFragmentManager().beginTransaction()
-                .add(android.R.id.content, detailFrag).commit();
+                    .add(android.R.id.content, detailFrag).commit();
+        }
+    }
+
+    @Override
+    public void finish() {
+        if (transitionReversed)
+            super.finish();
+        else {
+            transitionReversed = true;
+            ActivityCompat.finishAfterTransition(this);
         }
     }
 
