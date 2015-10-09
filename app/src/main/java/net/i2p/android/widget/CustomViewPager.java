30b86499cd7f7e9f2ffb3c3dcc186867d5ecdef2
str4d <str4d@mail.i2p>
2015-10-09 01:38:10 +0000

    Fix tab changing on rotate

--- app/src/main/java/net/i2p/android/widget/CustomViewPager.java
+++ app/src/main/java/net/i2p/android/widget/CustomViewPager.java
@@ -1,6 +1,10 @@
 package net.i2p.android.widget;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v4.os.ParcelableCompat;
+import android.support.v4.os.ParcelableCompatCreatorCallbacks;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -45,5 +49,68 @@ public class CustomViewPager extends ViewPager {
     public void updatePagingState() {
         if (!mEnabled && getCurrentItem() != 0)
             setCurrentItem(0);
+    }
+
+    public static class SavedState extends BaseSavedState {
+        boolean enabled;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeInt(enabled ? 1 : 0);
+        }
+
+        @Override
+        public String toString() {
+            return "CustomViewPager.SavedState{"
+                    + Integer.toHexString(System.identityHashCode(this))
+                    + " enabled=" + enabled + "}";
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = ParcelableCompat.newCreator(new ParcelableCompatCreatorCallbacks<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in, ClassLoader loader) {
+                return new SavedState(in, loader);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        });
+
+        SavedState(Parcel in, ClassLoader loader) {
+            super(in);
+            if (loader == null) {
+                loader = getClass().getClassLoader();
+            }
+            enabled = in.readInt() != 0;
+        }
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+        ss.enabled = mEnabled;
+        return ss;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        if (!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState ss = (SavedState)state;
+        super.onRestoreInstanceState(ss.getSuperState());
+
+        mEnabled = ss.enabled;
     }
 }
