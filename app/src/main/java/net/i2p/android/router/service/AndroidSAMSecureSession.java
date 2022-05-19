49e4fcc49c9255fac196744c4a43acb12601d3a9
idk <hankhill19580@gmail.com>
2022-05-19 15:41:48 -0400

    refactor SAMSecureSession implementation

--- app/src/main/java/net/i2p/android/router/service/AndroidSAMSecureSession.java
+++ app/src/main/java/net/i2p/android/router/service/AndroidSAMSecureSession.java
@@ -5,13 +5,21 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.DialogInterface;
+import android.content.res.AssetManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import net.i2p.android.router.R;
 import net.i2p.android.router.util.Util;
 import net.i2p.sam.*;
+import net.i2p.sam.SAMException;
+
+import java.util.Properties;
+
 /**
  * Implements SAMSecureSessionInterface on Android platforms using a Toast
  * as the interactive channel.
@@ -25,72 +33,64 @@ public class AndroidSAMSecureSession implements SAMSecureSessionInterface {
         mCtx = ctx;
     }
 
-    public Activity getActivity(){
-        Context aCtx = mCtx.getApplicationContext();
-        return getActivity(mCtx);
-    }
+    private class SAMSecureRunnable implements Runnable {
+        private int result = -1;
 
-    private Activity getActivity(Context baseContext)
-    {
-        if (baseContext == null)
-        {
-            Util.e("Base Context is Null");
-            return null;
+        @Override
+        public void run() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mCtx.getApplicationContext());
+
+            builder.setTitle(mCtx.getResources().getString(R.string.settings_confirm_sam));
+            builder.setMessage(mCtx.getResources().getString(R.string.settings_confirm_sam));
+
+            builder.setPositiveButton(mCtx.getResources().getString(R.string.settings_confirm_allow_sam), new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // set result=true and close the dialog
+                    result = 1;
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton(mCtx.getResources().getString(R.string.settings_confirm_deny_sam), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // set result=false
+                    result = 0;
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
         }
-        else if (baseContext instanceof ContextWrapper)
-        {
-            if (baseContext instanceof Activity)
-            {
-                Util.i("Base Context is Activity");
-                return (Activity) baseContext;
-            }
-            else
-            {
-                Util.i("Recursively seeking Main Context");
-                return getActivity(((ContextWrapper) baseContext).getBaseContext());
-            }
-        }
 
-        return null;
-    }
-    @Override
-    public boolean getSAMUserInput() {
-        final int[] approve = {-1};
-        Activity activity = getActivity();
-
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case DialogInterface.BUTTON_POSITIVE:
-                                approve[0] = 1;
-                                break;
-
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                approve[0] = 0;
-                                break;
-                        }
-                    }
-                };
-                AlertDialog.Builder builder = new AlertDialog.Builder(mCtx);
-                builder.setMessage(mCtx.getResources().getString(R.string.settings_confirm_sam)).setPositiveButton(mCtx.getResources().getString(R.string.settings_confirm_allow_sam), dialogClickListener)
-                        .setNegativeButton(mCtx.getResources().getString(R.string.settings_confirm_deny_sam), dialogClickListener).show();
-            }
-        });
-        //wait until we have input
-        while (approve[0] == -1) {
-            try {
+        public boolean isResult() {
+            while (result == -1) {
                 Util.i("Waiting on user to approve SAM connection");
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Util.e("Interrupted SAM approval, failing closed", e);
-                return false;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return false;
+                }
             }
+            if (result == 0)
+                return false;
+            if (result == 1)
+                return true;
+            return false;
         }
+    }
+    public Context getApplicationContext() {
+        return mCtx.getApplicationContext();
+    }
 
-        return (approve[0]==0) ? false : true;
+    @Override
+    public boolean approveOrDenySecureSession(Properties i2cpProps, Properties props) throws SAMException {
+        Handler handler = new Handler(Looper.getMainLooper());
+        SAMSecureRunnable ssr = new SAMSecureRunnable();
+        handler.post(ssr);
+        return false;
     }
 }
